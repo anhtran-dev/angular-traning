ac4f9213016d4cc6c32bd8fe16bcec829e9c6056
Andy <gs.hoanganh@gmail.com>
2020-10-30 16:53:06 +0700

    update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,22 +6,39 @@ import {Product} from '../models/product.class';
 })
 export class ProductService {
     public products = [
-        new Product(1, 'LOL', 4234, true),
-        new Product(1, 'LOL', 4234, true),
+        new Product(1, 'Technician', 44790, true),
+        new Product(2, 'Orchestrator', 97359, true),
+        new Product(3, 'Developer', 71600, false),
+        new Product(4, 'Producer', 41030, true),
     ];
 
     constructor() {
     }
 
-    getListProducts = () => {
-        return this.products;
+    getListProducts = (name?: string, price?: number) => {
+        let result: Product[] = this.products;
+        if (name) {
+            result = this.products.filter(item => {
+                return item.name.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+            });
+        }
+        if (price){
+            result = this.products.filter(item => {
+                return item.price === Number(price);
+            });
+        }
+        return result;
     };
 
     getProductById = (id) => {
+        let result = null;
         if (this.products.length > 0) {
-            return this.products.filter(item => item.id === id);
-        } else {
-            return null;
+            this.products.map(item => {
+                if (item.id === id) {
+                    result = item;
+                }
+            });
         }
-    }
+        return result;
+    };
 }
